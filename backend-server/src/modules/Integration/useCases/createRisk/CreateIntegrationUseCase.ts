db85d5d518371a2f273f6dc14c5dbb1b6f603635
davi paluch <davipaluch@icloud.com>
2024-10-20 23:26:50 -0300

    fix: adding the f&k*@g dots before object

--- backend-server/src/modules/Integration/useCases/createRisk/CreateIntegrationUseCase.ts
+++ backend-server/src/modules/Integration/useCases/createRisk/CreateIntegrationUseCase.ts
@@ -18,16 +18,7 @@ export class CreateIntegrationUseCase {
 
     //Risk
     const riskData: ICreateRisk = {
-      name: integrationData.name,
-      typeRisk: integrationData.typeRisk,
-      areaResponsibleIdentification: integrationData.areaResponsibleIdentification,
-      riskEntryDate: integrationData.riskEntryDate,
-      riskEntryDateWeek: integrationData.riskEntryDateWeek,
-      consequences: integrationData.consequences,
-      project: integrationData.project,
-      metier: integrationData.metier,
-      jalonAffected: integrationData.jalonAffected,
-      impactedJalonFuture: integrationData.impactedJalonFuture
+      ...integrationData
     };
     const riskUseCase = container.resolve(CreateRiskUseCase);
     const riskCreated = await riskUseCase.execute({ riskData });
@@ -35,23 +26,14 @@ export class CreateIntegrationUseCase {
     //Qualitative Analysis
     const qualitativeAnalysisData: ICreateQualitativeAnalysis = {
       idRisk: riskCreated.id,
-      probability: integrationData.probability,
-      impact: integrationData.impact,
-      riskRating: integrationData.riskRating,
-      impactRenault: integrationData.impactRenault
+      ...integrationData
     };
     const qualitativeAnalysisUseCase = container.resolve(CreateQualitativeAnalysisUseCase);
     const qualitativeAnalysisCreated = await qualitativeAnalysisUseCase.execute({ qualitativeAnalysisData });
 
     // Residual Risk
     const residualRiskData: ICreateResidualRisk = {
-      residualProbability: integrationData.residualProbability,
-      residualImpact: integrationData.residualImpact,
-      residualRiskRating: integrationData.residualRiskRating,
-      actionValidation: integrationData.actionValidation,
-      riskValidation: integrationData.riskValidation,
-      resolutionDate: integrationData.resolutionDate,
-      resolutionDateWeek: integrationData.resolutionDateWeek
+      ...integrationData
     };
     const residualRiskUseCase = container.resolve(CreateResidualRiskUseCase);
     const residualRiskCreated = await residualRiskUseCase.execute({ residualRiskData });
@@ -60,17 +42,7 @@ export class CreateIntegrationUseCase {
     const actionPlanData: ICreateActionPlan = {
       idRisk: riskCreated.id,
       idResidualRisk: residualRiskCreated.id,
-      strategy: integrationData.strategy,
-      action: integrationData.action,
-      pilotName: integrationData.pilotName,
-      initialPlanDate: integrationData.initialPlanDate,
-      initialPlanDateWeek: integrationData.initialPlanDateWeek,
-      alertDate: integrationData.alertDate,
-      alertDateWeek: integrationData.alertDateWeek,
-      resolutionTime: integrationData.resolutionTime,
-      time: integrationData.time,
-      comments: integrationData.comments,
-      isCapitalization: integrationData.isCapitalization
+      ...integrationData
     }
     const actionPlanUseCase = container.resolve(CreateActionPlanUseCase);
     const actionPlanCreated = await actionPlanUseCase.execute({ actionPlanData });
